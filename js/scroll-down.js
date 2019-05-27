261208a966cb4804d8cfcceea8bb55d53116db4a
Annika <annikawespe@gmail.com>
2019-05-27 18:00:54 +0200

    save

--- js/scroll-down.js
+++ js/scroll-down.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-    console.log('scrolling')
-    window.scrollBy(500,500);
-    window.scrollTo(0,10000);
 
+    function explode(){
+            console.log('scrolling')
+    
+    window.scrollBy(0,200);
+    didScroll = true;
+      }
+      setTimeout(explode, 2000);
 })
